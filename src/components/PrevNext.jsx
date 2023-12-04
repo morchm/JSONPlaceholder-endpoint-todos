0e912e4c7766e11371d9b8bd3e991d9361c7c2e1
morchm <129373609+morchm@users.noreply.github.com>
2023-12-04 15:15:35 +0100

    adding last tailwind touches

--- src/components/PrevNext.jsx
+++ src/components/PrevNext.jsx
@@ -9,14 +9,14 @@ export default function PrevNext({
   return (
     <div>
       <button
-        className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l m-1 rounded"
+        className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l m-1 rounded  disabled:opacity-50 disabled:cursor-not-allowed"
         onClick={() => setCurrentPage(currentPage - 1)}
         disabled={currentPage <= 0}
       >
         Prev
       </button>
       <button
-        className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l m-1 rounded"
+        className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l m-1 rounded  disabled:opacity-50 disabled:cursor-not-allowed"
         onClick={() => setCurrentPage(currentPage + 1)}
         disabled={currentPage + 1 >= Math.ceil(dataLength / itemsPerPage)}
       >
--- src/jsonplaceholder/PostsAdmin.jsx
+++ src/jsonplaceholder/PostsAdmin.jsx
@@ -6,7 +6,9 @@ import PrevNext from "../components/PrevNext";
 import ItemsPerPage from "../components/ItemsPerPage";
 
 // Icons - npm i react-icons
-import { FaEdit, FaPlus } from "react-icons/fa";
+import { FaEdit, FaPlus} from "react-icons/fa";
+import { FaTrashCan } from "react-icons/fa6";
+
 
 export default function PostsAdmin() {
   //Hent data der skal map'es
@@ -23,7 +25,7 @@ export default function PostsAdmin() {
   }
 
   return (
-    <div className="max-w-7xl m-auto">
+    <div className="max-w-3xl m-auto">
       <h1 className="text-center font-bold text-2xl m-5">
         JSONPlaceholder - Posts Admin
       </h1>
@@ -37,7 +39,7 @@ export default function PostsAdmin() {
                 <td></td>
                 <td></td>
                 <td></td>
-                <td><Link to={"/postcreate"} ><FaPlus/></Link></td>
+                <td><Link to={"/postcreate"} ><FaPlus className="text-2xl my-4 text-lime-600 hover:text-lime-700"/></Link></td>
             </tr>
           <tr>
             <td>ID</td>
@@ -52,8 +54,8 @@ export default function PostsAdmin() {
             <tr key={p.id}>
                 <td>{p.id}</td>
                 <td>{p.title}</td>
-                <td className="px-10"><Link to={"/post/" + p.id }><FaEdit className="hover:text-cyan-500 text-3xl"/></Link></td>
-                <td><button onClick={ ()=> handleDelete (p.id) } className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l m-1 rounded">Slet</button></td>
+                <td className="px-10"><Link to={"/post/" + p.id }><FaEdit className="hover:text-sky-700 text-3xl"/></Link></td>
+                <td><button onClick={ ()=> handleDelete (p.id) } className="bg-gray-300 hover:bg-red-700 hover:text-white text-gray-800 font-bold py-2 px-4 rounded-l m-1 rounded"><FaTrashCan/></button></td>
             </tr>
             ))}
         </tbody>
