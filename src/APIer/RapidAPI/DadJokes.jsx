96cb28862264136dfa8f617c67e9d31676f294a8
morchm <129373609+morchm@users.noreply.github.com>
2023-12-09 19:49:53 +0100

    Update Dadjokes

--- src/APIer/RapidAPI/DadJokes.jsx
+++ src/APIer/RapidAPI/DadJokes.jsx
@@ -7,9 +7,9 @@ export default function DadJokes() {
   const { makeRequest, isLoading, data, error } = useRequestData();
 
   const handleSubmit = e => {
-    makeRequest("https://dad-jokes.p.rapidapi.com/random/joke", "GET", null, {
+    makeRequest("https://dad-jokes-by-api-ninjas.p.rapidapi.com/v1/dadjokes", "GET", null, {
       'X-RapidAPI-Key': '05f10d1657msh9ee107d4bd0bdebp1ba22fjsn4b725733d903',
-      'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
+      'X-RapidAPI-Host': 'dad-jokes-by-api-ninjas.p.rapidapi.com'
         })
     }
 
@@ -19,19 +19,21 @@ export default function DadJokes() {
 
   return (
     <div className="max-w-7xl m-auto">
-    <h1 className="text-center font-bold text-2xl m-5">Far Jokes</h1>
-      {/* {isLoading && <Loader />} */}
+    <h1 className="text-center font-bold text-2xl m-5">Dad Jokes</h1>
+      {isLoading && <Loader />}
       {error && <Error />}
 
       {
-        data && 
+        data && data.map((e)=>(
+          
           <div className="card flex flex-col justify-center">
-            <h2 className="mb-6 text-3xl font-bold text-center">{data.type}</h2>
-            <p>{data.setup}</p>
+            <p className="text-center shadow-lg rounded-full m-auto p-4 border-2 my-2 text-3xl">{e.joke}</p>
             <button className="w-64 bg-gray-300 hover:bg-gray-500 rounded mx-2 p-1 my-2 text-center self-center" 
             onClick={handleSubmit}
-            >Ny joke</button>
+            >Tell me a new one! </button>
           </div>
+       
+        ))
       }
     </div>
   );
